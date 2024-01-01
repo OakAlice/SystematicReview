1a7372efce7cba497e1ee0342f4bf2a32994e2ba
OakAlice <oakleigh.wilson05@gmail.com>
2024-01-01 12:09:11 +1000

    trying to make ScienceDirect work... unsuccessful

--- README.md
+++ README.md
@@ -6,15 +6,22 @@ Input Variables and Run the Code
 * [UserInput.py](https://github.com/OakAlice/SystematicReview/blob/main/UserInput.py) creates the unique search strings from sets of keyword synonyms, user inputs all variables, then calls and runs all other code sections (individually below)
 
 Search the Databases
-* [ScholarStringToUrl.py](https://github.com/OakAlice/SystematicReview/blob/main/ScholarStringToUrl.py)  [SearchScholar.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchScholar.py) and [TidyScholar.py](https://github.com/OakAlice/SystematicReview/blob/main/TidyScholar.py) are for scraping and formatting for Google Scholar. This has a very high probability of being blocked and I was unable to use it.
+* [ScholarStringToUrl.py](https://github.com/OakAlice/SystematicReview/blob/main/ScholarStringToUrl.py)  [SearchScholar.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchScholar.py) and [TidyScholar.py](https://github.com/OakAlice/SystematicReview/blob/main/TidyScholar.py) are for scraping and formatting for Google Scholar. This has a very high probability of being blocked and I was unable to use it beyond the first 10 or so strings.
 * [SearchScopus.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchScopus.py)
 * [SearchPubMed.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchPubMed.py)
 * [SearchCORE.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchCORE.py)
 * [SearchArXiv.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchArXiv.py) # only include if you're searching for pre-prints too
-* [SearchScienceDirect.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchScienceDirect.py)
+* [SearchScienceDirect.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchScienceDirect.py) # not currently functional?
+* [SearchSemantic.py](https://github.com/OakAlice/SystematicReview/blob/main/SearchSemantic.py) # also not working
 
 Combining and Completing
 * [Combining.py](https://github.com/OakAlice/SystematicReview/blob/main/Combining.py) stitches together tidied csvs and removes cross-database duplication
 * [GettingAbstracts.py](https://github.com/OakAlice/SystematicReview/blob/main/GettingAbstracts.py) retrives the full abstract for papers that weren't retrived from the above databases (only works for some)
 
-And then you're done. Hopefully it's not too many papers.
+And then you're done. Hopefully they're good papers.
+
+## Installation
+Install dependencies
+```shell
+pip install -r requirements.txt
+```
--- UserInput.py
+++ UserInput.py
@@ -1,23 +1,23 @@
 # User input and code initialisation page
+# it works in theory, but many of the individual search systems don't work anymore..
 
 from itertools import product
 
 # enter the keywords to search for where each row is a concept with synonyms
-A1 = ['accelerometer', 'IMU'] # base
-A2 = ['activity', 'behaviour',] # synonyms
-A3 = ['machine learning', 'classification'] # synonyms
-B = ['wildlife', 'animal'] # synonyms
-C = ['unsupervised', 'clustering'] # synonyms
+A1 = ['accelerometer', 'IMU', 'accelerometry']
+A2 = ['activity recognition', 'behaviour',] 
+A3 = ['machine learning', 'classification'] 
+B = ['wildlife', 'animal']
+C = ['supervised', 'unsupervised']
 
 # define the concept combinations you'd want to search for
-combinations_a = list(product(A1, A2, A3, B)) # animal (probably supervised)
-combinations_b = list(product(A1, A2, A3, B, C)) # animal unsupervised
+combinations_a = list(product(A1, A2, A3, B, C))
 
-# they're added together to form one list
-search_strings = combinations_a + combinations_b
+# add together to form one list, if necessary
+search_strings = combinations_a
 
-# the parent directory where the results should be saved (they'll go into individual database folders)
-output_directory = "C:/Users/oakle/Documents/PhD docs/Systematic Results/DecemberRedo"
+# the parent directory where the results should be saved
+output_directory = "C:/Users/oakle/Documents/PhD docs/Systematic Results/DecemberRedo/Third_attempt"
 
 # number of articles per search from each of the strings
 Num_of_articles = 100
@@ -27,38 +27,40 @@ ScopusKey = 'fdbb42b4b0363feb81cf4551863b0279' # Elsevier's Scopus Search API
 PubMedEmail = 'oaw001@student.usc.edu.au' # PubMed email account
 COREKey = "XXXX" # for CORE Api
 SemanticKey = 'XBlMKbMLsO1pCQSj9zbQDaW7MrGOSvlS9OGo6cko' # for Semantic Scholar
-
+scienceDIrectKey = '9b54f4ad7b44a3e31256ec633cc7014c' # not working for some reason
 # The script is now ready to run
 
 # import the functions from each of the individual scripts
 
-from ScholarStringsToUrl import generate_scholar_urls
-from SearchScholar2 import QueryScholar
+#from ScholarStringsToUrl import generate_scholar_urls
+#from SearchScholar2 import QueryScholar
 #from TidyScholar import TidyingScholarResults
+
+#from SearchArXiv import QueryArxiv
+#from SearchCORE import QueryCore
 
 from SearchScopus import QueryScopus
 from SearchPubMed import QueryPubMed
-#from SearchArXiv import QueryArxiv
-#from SearchCORE import QueryCore
 from SearchScienceDirect import QueryScienceDirect
+from SearchSematic import QuerySemantic
 
 from Combining import CombiningDatabases
-from GettingAbstracts import get_abstracts
+#from GettingAbstracts import get_abstracts
 
 # call and run all the scripts with these variables
 def main():
-    urls = generate_scholar_urls(search_strings)
-    QueryScholar(urls, Num_of_articles, output_directory)
+    #urls = generate_scholar_urls(search_strings)
+    #QueryScholar(urls, Num_of_articles, output_directory)
     #TidyingScholarResults(output_directory)
+    #QueryArxiv(search_strings, Num_of_articles, output_directory)
+    #QueryCore(output_directory, search_strings, COREKey, Num_of_articles)
     
     #QueryScopus(ScopusKey, search_strings, output_directory, Num_of_articles)
     #QueryPubMed(search_strings, Num_of_articles, output_directory, PubMedEmail)
-    #QueryArxiv(search_strings, Num_of_articles, output_directory)
-    #QueryCore(output_directory, search_strings, COREKey, Num_of_articles)
-    #QuerySemantic(search_strings, offset=0, limit=Num_of_articles)
-    #QueryScienceDirect(search_strings, Num_of_articles, ScopusKey, output_directory)
+    #QuerySemantic(search_strings, output_directory, SemanticKey)
+    #QueryScienceDirect(search_strings, Num_of_articles, scienceDIrectKey, output_directory)
         
-    #CombiningDatabases(output_directory)
+    CombiningDatabases(output_directory)
     #get_abstracts(output_directory, PubMedEmail)
 
 if __name__ == "__main__":
